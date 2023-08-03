eadf93103575aeaabe821da7bafc70bd86d01631
Aileen Cleary <acleary@anl.gov>
2023-08-03 12:26:02 -0500

    Barty integration..

--- barty_node/barty_rest_node.py
+++ barty_node/barty_rest_node.py
@@ -1,8 +1,12 @@
 """The server that takes incoming WEI flow requests from the experiment application"""
 import json
+import time
+
 from argparse import ArgumentParser
 from contextlib import asynccontextmanager
-import time
+import ast
+import uvicorn
+
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.responses import JSONResponse
 
@@ -13,40 +17,18 @@ global barty, state
 local_ip = 'barty.alcf.anl.gov'
 local_port = 8000
 
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global barty, state
-    """Initial run function for the app, parses the worcell argument
-        Parameters
-        ----------
-        app : FastApi
-           The REST API app being initialized
-
-        Returns
-        -------
-        None"""
     try:
             state = "IDLE"
     except Exception as err:
             print(err)
             state = "ERROR"
-
-    # Yield control to the application
     yield
-
-    # Do any cleanup here
     pass
 
-
 app = FastAPI(lifespan=lifespan, )
-
-@app.post("/pumpA")
-def run_pathA():
-    barty_driver.forward("B", 100, 10)
-    return None
-
 
 @app.get("/state")
 def get_state():
@@ -63,13 +45,12 @@ def get_state():
 @app.get("/description")
 async def description():
     global barty, state
-    return JSONResponse(content={"State": state })
+    return JSONResponse(content={"State": state})
 
 @app.get("/resources")
 async def resources():
     global barty, state
-    return JSONResponse(content={"State": state })
-
+    return JSONResponse(content={"State": state})
 
 @app.post("/action")
 def do_action(
@@ -79,10 +60,47 @@ def do_action(
 
     global barty, state
     state = "BUSY"
-    if action_handle == 'pumpA':  
+    args = ast.literal_eval(action_vars)
+
+    if action_handle == "drain_ink_all_motors":  
         try:           
-            #sealer.seal()
-            time.sleep(15)  
+            barty_driver.drain_all(195) # Combined protocol lists A and B plate volume as 195mL.
+            response_content = {
+                    "action_msg": "StepStatus.Succeeded",
+                    "action_response": "True",
+                    "action_log": ""
+                }
+            state = "IDLE"
+            return JSONResponse(content=response_content)
+        except Exception as e:
+            response_content = {
+            "status": "failed",
+            "error": str(e),
+        }
+            state = "IDLE"
+            return JSONResponse(content=response_content)
+    
+    elif action_handle == "fill_ink_all_motors":  
+        try:           
+            barty_driver.refill_all(195)
+            response_content = {
+                    "action_msg": "StepStatus.Succeeded",
+                    "action_response": "True",
+                    "action_log": ""
+                }
+            state = "IDLE"
+            return JSONResponse(content=response_content)
+        except Exception as e:
+            response_content = {
+            "status": "failed",
+            "error": str(e),
+        }
+            state = "IDLE"
+            return JSONResponse(content=response_content)
+
+    elif action_handle == "refill_ink":  
+        try: 
+            barty_driver.refill(args['motors'])
             response_content = {
                     "action_msg": "StepStatus.Succeeded",
                     "action_response": "True",
@@ -102,3 +120,6 @@ def do_action(
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("barty_REST:app", host=local_ip, port=local_port, reload=True, ws_max_size=100000000000000000000000000000000000000)
+
+
+
