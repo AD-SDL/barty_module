d4eb911644a119214bd85c802e1c5cc00d896349
tginsbu1 <tsg44@cornell.edu>
2024-08-01 14:33:35 -0500

    updating module

--- barty_node/barty_rest_node.py
+++ barty_node/barty_rest_node.py
@@ -4,149 +4,42 @@
 from contextlib import asynccontextmanager
 
 import barty_driver
-import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from wei.modules.rest_module import RESTModule
+from wei.types.step_types import StepResponse
+from typing import List, Annotated
 
 global barty
 
 
 workcell = None
-global barty, state
 local_ip = "kirby.alcf.anl.gov"
 local_port = 8000
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    """
-    Manage fastapi app
-    """
-    global barty, state
-    try:
-        state = "IDLE"
-    except Exception as err:
-        print(err)
-        state = "ERROR"
-    yield
-    pass
-
-
-app = FastAPI(
-    lifespan=lifespan,
-)
-
-
-@app.post("/pumpA")
-def run_pathA():
-    barty_driver.forward("B", 100, 10)
-    return None
-
-
-@app.get("/state")
-def get_state():
-    global barty, state
-    if state != "BUSY":
-        pass
-        # barty.get_status()
-        # if barty.status_msg == 3:
-        #            msg.data = 'State: ERROR'
-        #            state = "ERROR"
-        # elif barty.status_msg == 0:
-        #           state = "IDLE"
-    return JSONResponse(content={"State": state})
-
-
-@app.get("/description")
-async def description():
-    global barty, state
-    return JSONResponse(content={"State": state})
-
-
-@app.get("/resources")
-async def resources():
-    global barty, state
-    return JSONResponse(content={"State": state})
-
-
-@app.post("/action")
-def do_action(
-    action_handle: str,
-    action_vars,
-):
-    global barty, state
-    state = "BUSY"
-
-    if action_handle == "drain_ink_all_motors":
-        try:
-            barty_driver.drain_all(
-                100
-            )  # Combined protocol lists A and B plate volume as 195mL.
-            response_content = {
-                "action_msg": "Successfully drained reservoirs",
-                "action_response": "succeeded",
-                "action_log": "",
-            }
-            state = "IDLE"
-            print("finished")
-            return JSONResponse(content=response_content)
-        except Exception as e:
-            response_content = {
-                "action_msg": "",
-                "action_response": "failed",
-                "action_log": str(e),
-            }
-            print(e)
-            state = "IDLE"
-            return JSONResponse(content=response_content)
-
-    elif action_handle == "fill_ink_all_motors":
-        try:
-            barty_driver.refill_all(60)
-            response_content = {
-                "action_msg": "Successfully filled reservoirs",
-                "action_response": "succeeded",
-                "action_log": "",
-            }
-            print("finished")
-            state = "IDLE"
-            return JSONResponse(content=response_content)
-        except Exception as e:
-            response_content = {
-                "action_msg": "",
-                "action_response": "failed",
-                "action_log": str(e),
-            }
-            state = "IDLE"
-            return JSONResponse(content=response_content)
-
-    elif action_handle == "refill_ink":
-        try:
-            barty_driver.refill(action_vars["motors"], 5)
-            response_content = {
-                "action_msg": "run pumpB for" + actions_vars,
-                "action_response": "succeeded",
-                "action_log": "",
-            }
-            state = "IDLE"
-            return JSONResponse(content=response_content)
-        except Exception as e:
-            response_content = {
-                "action_response": "failed",
-                "action_log": str(e),
-                "action_msg": "",
-            }
-            state = "IDLE"
-            return JSONResponse(content=response_content)
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(
-        "barty_rest_node:app",
-        host=local_ip,
-        port=local_port,
-        reload=True,
-        ws_max_size=100000000000000000000000000000000000000,
+rest_module = RESTModule(
+        name="pf400_node",
+        version="0.0.1",
+        description="A node to control the pf400 plate moving robot",
+        model="pf400",
     )
+
+@rest_module.action(name="drain_ink_all_motors", description="drains 100ml of ink on all pumps")
+def drain_all():        
+        barty_driver.drain_all(
+            100
+        )  # Combined protocol lists A and B plate volume as 195mL.
+        
+        return StepResponse.step_succeeded()
+@rest_module.action(name="fill_ink_all_motors", description="fills 60ml of ink on all pumps")
+def fill_all():               
+            barty_driver.refill_all(60)
+            return StepResponse.step_succeeded()
+
+@rest_module.action(name="refill_ink", description="fills 5ml of ink on target pumps")
+def refill_target( motors: Annotated[List[int], "motors to run"]): 
+            barty_driver.refill(motors, 5)
+            return StepResponse.step_succeeded()
+       
+
+
+rest_module.start()
