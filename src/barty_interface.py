5b18e5b0be1852f991614e82d7407d1364c2c8c4
Casey Stone <cstone@anl.gov>
2025-03-26 12:57:20 -0500

    Updating to madsci v0.1.7

--- src/barty_interface.py
+++ src/barty_interface.py
@@ -1,26 +1,28 @@
 """A Node i"""
 
-from typing import Optional
-
-from madsci.client.event_client import EventClient
 # from madsci.common.types.action_types import ActionFailed, ActionSucceeded
 # from madsci.common.types.node_types import RestNodeConfig
 # from madsci.node_module.abstract_node_module import action
 # from madsci.node_module.rest_node_module import RestNode
-
 import time
+from typing import Optional
+
 import RPi.GPIO as gpio
+from madsci.client.event_client import EventClient
 
 
 class BartyInterface:
     """An interface to Barty the bartender robot"""
+
+    cancel_flag: bool = False
+    pause_flag: bool = False
 
     def __init__(self, logger: Optional[EventClient] = None) -> "BartyInterface":
         """Initialize the Barty Interface"""
         self.logger = logger if logger else EventClient()
 
         # Using the pin numbers (inside) instead of GPIO (outside)
-        gpio.setmode(gpio.BOARD)  
+        gpio.setmode(gpio.BOARD)
         gpio.setwarnings(False)
 
         self.motors = {
@@ -31,7 +33,6 @@ class BartyInterface:
         }
 
         self.logger.log("Barty initialized and connection open")
-
 
     def initialize_motors(self, motor):
         """Initialize the motors."""
@@ -51,10 +52,7 @@ class BartyInterface:
 
         self.logger.log(f"Initialized motor: {self.motors[motor]}")
 
-        return   # TODO: Don't need to tell it to return here right? won't it just do it?
-    
-    
-    def forward(self, lis_motors, speed, second):
+    def forward(self, lis_motors, speed, seconds):
         """Move the motors forward."""
         for motor in lis_motors:
             self.initialize_motors(motor)
@@ -63,7 +61,7 @@ class BartyInterface:
             gpio.output(self.motors[motor]["f"], False)
             gpio.output(self.motors[motor]["r"], True)
             self.motors[motor]["pwm"].ChangeDutyCycle(speed)
-        time.sleep(second)
+        time.sleep(seconds)
 
         for motor in lis_motors:
             self.motors[motor]["pwm"].stop()
@@ -71,7 +69,6 @@ class BartyInterface:
         gpio.cleanup()
 
         self.logger.log("Moved motor forward")
-
 
     def backward(self, lis_motors, speed, second):
         """Move the motors backward."""
@@ -92,7 +89,6 @@ class BartyInterface:
 
         self.logger.log("Moved motor backward")
 
-
     def refill(self, lis_motors, vol):
         """Drive the specified motors forward to refill specific ink reservoirs."""
         norm_speed = 1.427  # 1.5 mL/s at DC=100, f=50.
@@ -100,7 +96,6 @@ class BartyInterface:
         self.forward(lis_motors, 100, duration)
 
         self.logger.log(f"Refilled motors {lis_motors} by volume {vol}")
-
 
     def drain(self, lis_motors, vol):
         """Drive the specified motors backward to drain specific ink reservoirs."""
@@ -110,14 +105,12 @@ class BartyInterface:
 
         self.logger.log(f"Drained motors {lis_motors} by volume {vol}")
 
-
     def refill_all(self, vol):
         """Drive all motors forward to refill all the ink reservoirs."""
         lis_motors = ["motor_1", "motor_2", "motor_3", "motor_4"]
         self.refill(lis_motors, vol)
 
         self.logger.log(f"Refilled all reservoirs with volume: {vol}")
-
 
     def drain_all(self, vol):
         """Drive all motors backward to drain all the ink reservoirs."""
@@ -127,35 +120,5 @@ class BartyInterface:
         self.logger.log(f"Drained all reservoirs by volume: {vol}")
 
 
-    # def run_command(self, command: str, fail: bool = False) -> bool:
-    #     """Run a command on the test interface."""
-    #     self.logger.log(f"Running command {command}.")
-    #     if fail:
-    #         self.logger.log(f"Failed to run command {command}.")
-    #         return False
-    #     return True
-
-
-    # QUESTIONS: 
-        # should each method be returning a True False here?
-        # do I need an if main at the end here?
-
-
 if __name__ == "__main__":
     barty = BartyInterface()
-    
-    
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
--- src/barty_node.py
+++ src/barty_node.py
@@ -1,22 +1,12 @@
 """A Node implementation to use in automated tests."""
 
-from itertools import count
-from typing import Optional
+from typing import Annotated, List
 
 from madsci.client.event_client import EventClient
-from madsci.common.types.action_types import ActionFailed, ActionSucceeded, ActionRunning, ActionRequest
+from madsci.common.types.action_types import ActionSucceeded
 from madsci.common.types.node_types import RestNodeConfig
 from madsci.node_module.abstract_node_module import action
 from madsci.node_module.rest_node_module import RestNode
-from madsci.common.utils import threaded_task
-from madsci.common.types.base_types import Error
-
-from typing import Annotated, List
-
-import barty_driver
-from fastapi.datastructures import State
-from wei.modules.rest_module import RESTModule
-from wei.types.step_types import StepResponse
 
 from barty_interface import BartyInterface
 
@@ -25,22 +15,7 @@ class BartyNodeConfig(RestNodeConfig):
     """Configuration for Barty the bartender robot."""
 
     # TODO: Add configs
-    # __test__ = False
 
-    # test_required_param: int
-    # """A required parameter."""
-    # test_optional_param: Optional[int] = None
-    # """An optional parameter."""
-    # test_default_param: int = 42
-    # """A parameter with a default value."""
-
-    # TODO: where to put this information?
-    # barty_module = RESTModule(
-    # name="Barty",
-    # version="1.0.0",
-    # description="A python module controlling Barty the amazing bartending robot",
-    # model="Barty v1",
-    # )
 
 class BartyNode(RestNode):
     """A node for Barty the bartender robot."""
@@ -51,9 +26,11 @@ class BartyNode(RestNode):
     logger = EventClient()
 
     def startup_handler(self) -> None:
-        """Initialize or reinitialize Barty. """
+        """Initialize or reinitialize Barty."""
         self.logger.log("Barty initializing...")
-        self.barty_interface = BartyInterface(logger=self.logger)  # TODO: would this work variable wise?
+        self.barty_interface = BartyInterface(
+            logger=self.logger
+        )  # TODO: would this work variable wise?
         self.logger.log("Barty initialized!")
 
     def shutdown_handler(self) -> None:
@@ -65,49 +42,47 @@ class BartyNode(RestNode):
 
     def state_handler(self) -> None:
         """Periodically called to update the current state of the node."""
-        
 
-    def pause(self) -> None:    # TODO: implement
+    def pause(self) -> None:  # TODO: implement
         """Pause the node."""
         self.logger.log("Pausing node...")
         self.node_status.paused = True
         self.logger.log("Node paused.")
         return True
 
-    def resume(self) -> None:    # TODO: implement
+    def resume(self) -> None:  # TODO: implement
         """Resume the node."""
         self.logger.log("Resuming node...")
-        self.node_status.paused = Falsedrain_all
+        self.node_status.paused = False
         self.logger.log("Node resumed.")
         return True
 
-    def shutdown(self) -> None:   # TODO: implement
+    def shutdown(self) -> None:  # TODO: implement
         """Shutdown the node."""
         self.shutdown_handler()
         return True
 
-    def reset(self) -> None:     # TODO: implement
+    def reset(self) -> None:  # TODO: implement
         """Reset the node."""
         self.logger.log("Resetting node...")
         result = super().reset()
         self.logger.log("Node reset.")
         return result
 
-    def safety_stop(self) -> None:   # TODO implement
+    def safety_stop(self) -> None:  # TODO implement
         """Stop the node."""
         self.logger.log("Stopping node...")
         self.node_status.stopped = True
         self.logger.log("Node stopped.")
         return True
 
-    def cancel(self) -> None:     # TODO: implement
+    def cancel(self) -> None:  # TODO: implement
         """Cancel the node."""
         self.logger.log("Canceling node...")
         self.node_status.cancelled = True
         self.logger.log("Node cancelled.")
         return True
-    
-    
+
     ### ACTIONS ###
     @action(
         name="drain_ink_all_motors",
@@ -115,49 +90,28 @@ class BartyNode(RestNode):
     )
     def drain_all(
         self,
-        action: ActionRequest,
         amount: Annotated[int, "Amount of ink to drain, in milliliters"] = 100,
     ):
         """Drains specified amount of liquid from all motors"""
 
-        @threaded_task
-        def run_drain():
-            try:
-                self.barty_interface.drain_all(int(amount))
-                self._extend_action_history(ActionSucceeded(action_id=action.action_id))
-            except Exception as e:
-                self._extend_action_history(ActionFailed(action_id=action.action_id, errors=Error.from_exception(e)))
-        run_drain()
-
-        return ActionRunning()
-    
+        self.barty_interface.drain_all(int(amount))
+        return ActionSucceeded()
 
     @action(
         name="fill_ink_all_motors",
         description="fills the specified amount of ink on all pumps",
     )
     def fill_all(
-        self, 
-        action: ActionRequest,
-        amount: Annotated[int, "Amount of ink to fill, in milliliters"] = 60
+        self, amount: Annotated[int, "Amount of ink to fill, in milliliters"] = 60
     ):
         """Refills the specified amount of liquid from all motors"""
 
-        @threaded_task
-        def run_refill():
-            try:
-                self.barty_interface.refill_all(int(amount))
-                self._extend_action_history(ActionSucceeded(action_id=action.action_id))
-            except Exception as e:
-                self._extend_action_history(ActionFailed(action_id=action.action_id, errors=Error.from_exception(e)))
-        run_refill()
+        self.barty_interface.refill_all(int(amount))
+        return ActionSucceeded()
 
-        return ActionRunning()
-    
-
-    action(
-        name="refill_ink", 
-        description="fills the specified amount of ink on target pumps"
+    @action(
+        name="refill_ink",
+        description="fills the specified amount of ink on target pumps",
     )
     def refill_target(
         self,
@@ -172,6 +126,3 @@ class BartyNode(RestNode):
 if __name__ == "__main__":
     barty_node = BartyNode()
     barty_node.start_node()
-
-
-
